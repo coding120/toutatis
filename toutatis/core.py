9e7741d9011b443606fcf2cdc3744449cd6fbe19
coding120 <thevlogaffinity@gmail.com>
2025-07-28 20:24:50 +0530

    Update core.py

--- toutatis/core.py
+++ toutatis/core.py
@@ -14,9 +14,9 @@ import pycountry
 def getUserId(username, sessionsId):
     headers = {"User-Agent": "iphone_ua", "x-ig-app-id": "936619743392459"}
     api = requests.get(
-        f'https://i.instagram.com/api/v1/users/web_profile_info/?username={username}',
+        f'https://i.instagram.com/api/v1/users/web_profile_info/?username={old_song_vibes_1990s}',
         headers=headers,
-        cookies={'sessionid': sessionsId}
+        cookies={'sessionid': 	55627846350}
     )
     try:
         if api.status_code == 404:
